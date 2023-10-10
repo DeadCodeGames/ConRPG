53c2df902f81814164415f8a03dfde90a945d9d5
Richard Kužela <djmaker@centrum.sk>
2023-10-10 09:52:22 +0200

    Add files via upload

---
+++ conrpg.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+	srand(time(0));
+	cout << "                                          W E L C O M E    T O" << endl;
+ 	cout << " .----------------.    .----------------.  .----------------.  .-----------------.   .----------------. " << endl << "| .--------------. |  | .--------------. || .--------------. || .--------------. |  | .--------------. |" << endl << "| |              | |  | |     ______   | || |     ____     | || | ____  _____  | |  | |              | |" << endl << "| |              | |  | |   .' ___  |  | || |   .'    `.   | || ||_   \\|_   _| | |  | |              | |" << endl << "| |    ______    | |  | |  / .'   \\_|  | || |  /  .--.  \\  | || |  |   \\ | |   | |  | |    ______    | |" << endl << "| |   |______|   | |  | |  | |         | || |  | |    | |  | || |  | |\\ \\| |   | |  | |   |______|   | |" << endl << "| |              | |  | |  \\ `.___.'\\  | || |  \\  `--'  /  | || | _| |_\\   |_  | |  | |              | |" << endl << "| |              | |  | |   `._____.'  | || |   `.____.'   | || ||_____|\\____| | |  | |              | |" << endl << "| |              | |  | |              | || |              | || |              | |  | |              | |" << endl << "| '--------------' |  | '--------------' || '--------------' || '--------------' |  | '--------------' |" << endl << " '----------------'    '----------------'  '----------------'  '----------------'    '----------------' ";
+	cout << endl << "                                           The C++ Console RPG";
+	cout << endl << "                                         Press [ANY KEY] to Begin";
+	_getch();
+	cout << endl << "   Hello, Player!" << endl << "  ";
+	Sleep(1000);
+	cout << " .";  Sleep(500); cout << "."; Sleep(500); cout << "." << endl << "  "; Sleep(1000);
+	cout << " Your name isn't Player, is it?" << endl << "  ";
+	Sleep(1500);
+	cout << " What's your name?" << endl << "  [ENTER YOUR NAME NOW]" << endl << "   ";
+	string playerName;
+	getline(cin, playerName);
+	if (!playerName.empty()) {
+		cout << "   Nice to meet you, " << playerName << "!";;
+	} else {
+		cout << "   You"; Sleep(1000); cout << "."; Sleep(500); cout << "."; Sleep(500); cout << "."; Sleep(1000); cout << " Don't have a name?" << endl << "   I will give you one more chance to tell me your name." << endl;
+		getline(cin, playerName);
+		if (!playerName.empty()) {
+		cout << "   At last! Nice to meet you, " << playerName << "!";;
+	} else {
+		cout << "   You sure?" << endl; Sleep(1500);
+		cout << "   That's"; Sleep(1000); cout << "."; Sleep(500); cout << "."; Sleep(500); cout << "."; Sleep(1000); cout << " sad." << endl; Sleep(1000);
+		cout << "   I still need something to call you by, though." << endl; Sleep(750);
+		cout << "   Will Player suffice?" << endl; Sleep(1000);
+		cout << "   I think it will be alright." << endl << "[PRESS ANY KEY TO CONTINUE]"; _getch(); system("cls");
+		
+	};
+}
+}
