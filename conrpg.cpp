3aefcb9970693bb656011c52208fcf4527da37e6
SlovakTastic <134318702+SlovakTastic@users.noreply.github.com>
2024-01-23 12:47:46 +0100

    Update conrpg.cpp

--- conrpg.cpp
+++ conrpg.cpp
@@ -7,7 +7,10 @@
 #include <conio.h>
 #include <stdlib.h>
 using namespace std;
-string input, tb;
+
+string input, tb, decision1, decision1sub1;
+int a, b=0;
+bool isAlive = true;
 void clearLine(){
 	cout<<"\r                                              \r";
 }
@@ -98,7 +101,7 @@ void skip(int time){
 void getInput() {
 	cout << ">>    ";
 	getline(cin, input);
-	Sleep(1000);
+	skip(1000);
 }
 void flushInput() {
 		while(_kbhit()) {
@@ -141,7 +144,6 @@ int ambush(){
   si.cb = sizeof(si);
   ZeroMemory(&pi, sizeof(pi));
 
-  // Start the child process in a new console window.
   if (CreateProcess(NULL, const_cast<char*>(programName), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
       cout << "Launching combat program in a new console window..." << endl;
       WaitForSingleObject(pi.hProcess, INFINITE); 
@@ -165,7 +167,80 @@ int ambush(){
 	cout << "ono to funguje...";
 	Sleep(3000);
 }
+
+// Space saving
+
+void d1n(){
+	if(a==0) truenarrator("\t  Trees, interesting choice. You come closer to the trees but you don't find anything.", 50);
+	if(a==1) truenarrator("\t  For real? You visit the trees again, and... you don't find anything.", 50);
+	if(a==2) truenarrator("\t  There is something weird about those trees. They're... trees.", 50);
+	if(a>2){
+		truenarrator("\t  You visit the trees once more. Once again, you find nothin-", 50); cout << endl;
+		truenarrator("\t  w- what's that sound? It seems like... you have awakened something. It's angry.", 75); cout << endl; skip(300);
+		truenarrator("\t  Behold Groot, the tree [BOSS] [10M HP] You stand no chance against this beast as it one hits you", 50); cout << endl;
+		truenarrator("\t  before the fight even has a chance to begin.", 50); skip(400); cout << endl;
+		truenarrator("\t  You get the tree picker ending.", 50); cout << endl;
+		cout << "\t  [Press F to load the latest save]";
+		while(true){
+			if(_kbhit()){
+		  	char key = _getch();
+		  	if(key=='f'){
+		  		isAlive = false;
+		  		break;
+				}
+			}
+		}
+		
+	}
+}
+
+// Decisions
+
+void d1(){
+	truenarrator("\t  You look around you. You see the following:", 50);
+	cout << endl;
+	truenarrator("\t  1) Trees",50); cout << endl; truenarrator("\t  2) Trees", 50); cout << endl; truenarrator("\t  3) Trees", 50); cout << endl; truenarrator("\t  4) A path", 50); cout << endl; truenarrator("\t  5) A strange place between the trees, where abandoned stuff can be seen.", 50); skip(500); cout << endl << endl; truenarrator("\t  What do you choose?", 50);
+	cout << endl;
+}
+
 // Sequences
+void introsequence(){
+	cout << "                                          W E L C O M E    T O" << endl;
+ 	cout << " .----------------.    .----------------.  .----------------.  .-----------------.   .----------------. " << endl << "| .--------------. |  | .--------------. || .--------------. || .--------------. |  | .--------------. |" << endl << "| |              | |  | |     ______   | || |     ____     | || | ____  _____  | |  | |              | |" << endl << "| |              | |  | |   .' ___  |  | || |   .'    `.   | || ||_   \\|_   _| | |  | |              | |" << endl << "| |    ______    | |  | |  / .'   \\_|  | || |  /  .--.  \\  | || |  |   \\ | |   | |  | |    ______    | |" << endl << "| |   |______|   | |  | |  | |         | || |  | |    | |  | || |  | |\\ \\| |   | |  | |   |______|   | |" << endl << "| |              | |  | |  \\ `.___.'\\  | || |  \\  `--'  /  | || | _| |_\\   |_  | |  | |              | |" << endl << "| |              | |  | |   `._____.'  | || |   `.____.'   | || ||_____|\\____| | |  | |              | |" << endl << "| |              | |  | |              | || |              | || |              | |  | |              | |" << endl << "| '--------------' |  | '--------------' || '--------------' || '--------------' |  | '--------------' |" << endl << " '----------------'    '----------------'  '----------------'  '----------------'    '----------------' ";
+	cout << endl << "                                           The C++ Console RPG";
+	cout << endl << "                                         Press [ANY KEY] to Begin";
+	_getch();
+	cout << endl; narrator("Hello Player", 100); skip(1000); cout << endl;
+	narrator("...", 500); skip(1000); cout << endl;
+	narrator("Your name isn't Player, is it?", 80); skip(1500); cout << endl;
+	narrator("What's your name?", 80); skip(1000); cout << endl << "      [ENTER YOUR NAME NOW]" << endl;
+	string playerName;
+	flushInput();
+	getInput(); playerName = input;
+	if (!playerName.empty()) {
+		narrator("Nice to meet you, "+playerName+"!", 100);
+	} else {
+		narrator("You...", 500); narratorclean(" don't have a name?", 125); skip(500); narratorclean(" I will give you one more chance to give me your name.", 80); cout << endl; 
+		flushInput();
+		getInput(); playerName = input;
+		if (!playerName.empty()) {
+		narrator("At last, nice to meet you, "+playerName+"!", 80);
+	} else {
+		narrator("You sure?", 125); cout << endl; skip(1000);
+		narrator("That's", 225); narratorclean("...", 500); skip(500); narratorclean(" sad.", 200); cout << endl; skip(750);
+		narrator("I still need something to call you by, though", 80); cout << endl; skip(500);
+		narrator("Will Player suffice?", 75); skip(500); narratorclean(" I think it will be alright.", 75);
+	}
+}
+	skip(1000);
+	cout << endl << "      [PRESS ANY KEY TO START THE GAME]";
+	while(true){
+		if(_kbhit()){
+			system("cls");
+			break;
+		}
+	}
+}
 void sequence1(){
 	Sleep(3500);
 	MCS("Where am I?", 80); narratorclean(" How did I get here?",80); skip(1000);
@@ -205,48 +280,111 @@ void sequence1(){
 	narrator("Does it look like you have a choice? What else do you want to do?", 75); skip(1500);
 	cout << endl;
 	cout << endl;
-	truenarrator("      You look around you. You see the following:", 50);
+	d1();
+	while(true){
+		if(a>0 || b>0) d1();
+		cout << endl << endl;
+		flushInput();
+		cout << ">>    ";
+		getline(cin, decision1);
+		cout << endl << endl;
+		if(decision1=="1"){
+			MCS("I want to see the trees.", 80);
+			cout << endl << endl;
+			d1n();
+			if(!isAlive){
+				system("cls");
+				sequence1();
+				break;
+			}
+			skip(500);
+			cout << endl << endl;
+			a++;
+			continue;
+		}
+		else if(decision1=="2"){
+			MCS("I want to see those trees.", 80);
+			cout << endl << endl;
+			d1n();
+			if(!isAlive){
+				system("cls");
+				sequence1();
+				break;
+			}
+			skip(500);
+			cout << endl << endl;
+			a++;
+			continue;
+		}
+		else if(decision1=="3"){
+			MCS("I must see these trees.", 80);
+			cout << endl << endl;
+			d1n();
+			if(!isAlive){
+				system("cls");
+				sequence1();
+				break;
+			}
+			skip(500);
+			cout << endl << endl;
+			a++;
+			continue;		
+		}
+		else if(decision1=="4"){
+			MCS("I would like to see where this path leads.", 80);
+			cout << endl << endl;
+			truenarrator("\t  Okay, you decide you want to see the destination of this path. Are you sure you don't want to check\n\t  the abandoned place where your stuff might lay out first?", 50);
+			cout << endl << endl;
+			skip(250);
+			truenarrator("\t  1) Go through with your initial decision\n\t  2) Abort", 50);
+			cout << endl << endl << endl << ">>    ";
+			flushInput();
+			getline(cin, decision1sub1);
+			if(decision1sub1=="1"){
+				MCS("I really want to know where it leads.", 80);
+				cout << endl << endl << endl;
+				truenarrator("\t  Very well, you decide to embark on an adventurous journe- nevermind, you die. Reason: UNKN0WN", 50);
+				skip(400);
+				cout << endl << endl << endl;
+				truenarrator("\n\t  [Press F to pay respects(load the last save)]", 50);
+				while(true){
+					if(_kbhit()){
+				  	char key = _getch();
+				  	if(key=='f'){
+				  		break;
+						}
+					}
+				}
+				system("CLS");
+				sequence1();
+				break;
+			}
+			else if(decision1sub1=="2"){
+				MCS("I think we should check that strange abandoned place first.", 80);
+				cout << endl << endl;
+				truenarrator("\t  At last, you decide to check the strange place with abandoned stuff out. You see the following:",50); truenarrator("\n\t  - Tea",50); truenarrator("\n\t  - An iron sword",50); truenarrator("\n\t  - Golden armor with a low durability",50); truenarrator("\n\t  - A shield",50); truenarrator("\n\t  - Elixir of Love",50); truenarrator("\n\t  - A stick",50); truenarrator("\n\t  - Bandage x3",50); truenarrator("\n\t  - A small backpack", 50);
+				break;
+			}
+		}
+		else if(decision1=="5"){
+			MCS("I would like to check the strange abandoned place out first.", 80);
+			cout << endl << endl;
+				truenarrator("\t  Great choice, better than \"trees\" atleast, you near the abandoned things and find the following:",50); truenarrator("\n\t  - Tea",50); truenarrator("\n\t  - An iron sword",50); truenarrator("\n\t  - Golden armor with a low durability",50); truenarrator("\n\t  - A shield",50); truenarrator("\n\t  - Elixir of Love",50); truenarrator("\n\t  - A stick",50); truenarrator("\n\t  - Bandage x3",50); truenarrator("\n\t  - A small backpack", 50);			break;
+		}
+		else{
+			truenarrator("\t  Invalid choice. Choose again.", 50);
+			cout << endl << endl;
+			b++;
+			continue;
+		}
+	}
 	cout << endl << endl;
-	truenarrator("\t  1) Trees",50); cout << endl; truenarrator("\t  2) Trees", 50); cout << endl; truenarrator("\t  3) Trees", 50); cout << endl; truenarrator("\t  4) A path", 50); cout << endl; truenarrator("\t  5) A strange place between the trees, where abandoned stuff can be seen.", 50); skip(500); cout << endl << endl; truenarrator("\t  What do you choose?", 50);
-	cout << endl;	
+	narrator("Great.", 100); skip(250); narratorclean(" These things will come in handy. Grab them and let's get out of here.", 80); skip(1000);
+	
 }
 int main(){
 	srand(time(0));
-	cout << "                                          W E L C O M E    T O" << endl;
- 	cout << " .----------------.    .----------------.  .----------------.  .-----------------.   .----------------. " << endl << "| .--------------. |  | .--------------. || .--------------. || .--------------. |  | .--------------. |" << endl << "| |              | |  | |     ______   | || |     ____     | || | ____  _____  | |  | |              | |" << endl << "| |              | |  | |   .' ___  |  | || |   .'    `.   | || ||_   \\|_   _| | |  | |              | |" << endl << "| |    ______    | |  | |  / .'   \\_|  | || |  /  .--.  \\  | || |  |   \\ | |   | |  | |    ______    | |" << endl << "| |   |______|   | |  | |  | |         | || |  | |    | |  | || |  | |\\ \\| |   | |  | |   |______|   | |" << endl << "| |              | |  | |  \\ `.___.'\\  | || |  \\  `--'  /  | || | _| |_\\   |_  | |  | |              | |" << endl << "| |              | |  | |   `._____.'  | || |   `.____.'   | || ||_____|\\____| | |  | |              | |" << endl << "| |              | |  | |              | || |              | || |              | |  | |              | |" << endl << "| '--------------' |  | '--------------' || '--------------' || '--------------' |  | '--------------' |" << endl << " '----------------'    '----------------'  '----------------'  '----------------'    '----------------' ";
-	cout << endl << "                                           The C++ Console RPG";
-	cout << endl << "                                         Press [ANY KEY] to Begin";
-	_getch();
-	cout << endl; narrator("Hello Player", 100); skip(1000); cout << endl;
-	narrator("...", 500); skip(1000); cout << endl;
-	narrator("Your name isn't Player, is it?", 80); skip(1500); cout << endl;
-	narrator("What's your name?", 80); skip(1000); cout << endl << "      [ENTER YOUR NAME NOW]" << endl;
-	string playerName;
-	flushInput();
-	getInput();
-	if (!input.empty()) {
-		narrator("Nice to meet you, "+input+"!", 100);
-	} else {
-		narrator("You...", 500); narratorclean(" don't have a name?", 125); skip(500); narratorclean(" I will give you one more chance to give me your name.", 80); cout << endl; 
-		flushInput();
-		getInput();
-		if (!input.empty()) {
-		narrator("At last, nice to meet you, "+input+"!", 80);
-	} else {
-		narrator("You sure?", 125); cout << endl; skip(1000);
-		narrator("That's", 225); narratorclean("...", 500); skip(500); narratorclean(" sad.", 200); cout << endl; skip(750);
-		narrator("I still need something to call you by, though", 80); cout << endl; skip(500);
-		narrator("Will Player suffice?", 75); skip(500); narratorclean(" I think it will be alright.", 75);
-	}
-}
-	skip(1000);
-	cout << endl << "      [PRESS ANY KEY TO START THE GAME]";
-	while(true){
-		if(_kbhit()){
-			system("cls");
-			break;
-		}
-	}
+	introsequence();
 	sequence1();
 //	cout << "Now let's test multiple console windows..." << endl; Sleep(3000);
 //	ambush(); 
