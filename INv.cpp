c488ce47ae0b091638d0f2a196ffe14152becc8d
Zemk0 <147710287+Zemk0@users.noreply.github.com>
2024-03-01 14:00:06 +0100

    Update INv.cpp
    added some things

--- INv.cpp
+++ INv.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <conio.h>
+#include <Windows.h>
 using namespace std;
 void xpbar(double& perlevel) {
 	if (perlevel == 0) {
@@ -183,7 +184,8 @@ void xpbar(double& perlevel) {
 
 
 }
-void inventorystart(double& phealth, double& hhealth, double& pstrenght, int& hcapacity, double& pendurance, double& hspeed, int& level, int& hdistance, double& perlevel, string hname[]) {
+void inventorystart(double& phealth, double& hhealth, double& pstrenght, int& hcapacity, double& pendurance, double& hspeed, int& level, int& hdistance, double& perlevel, string hname) {
+	
 	system("cls");
 	cout << ",--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--," << endl << "|  _____ ____  _____ ____   ____ ________ ____  _____ _________   ___   _______  ____,_____  |" << endl << "* |_   _|_   \\|_   _|_  _| |_  _|_   __  |_   \\|_   _|  _   _  |.'   `.|_   __ \\|_  _||_  _| *" << endl << "|   | |   |   \\ | |   \\ \\   / /   | |_ \\_| |   \\ | | |_/ | | \\_/  .-.  \\ | |__) | \\ \\  / /   |" << endl << "*   | |   | |\\ \\| |    \\ \\ / /    |  _| _  | |\\ \\| |     | |   | |   | | |  __ /   \\ \\/ /    *" << endl << "|  _| |_ _| |_\\   |_    \\ ' /    _| |__/ |_| |_\\   |_   _| |_  \\  `-'  /_| |  \\ \\_ _|  |_    |" << endl << "* |_____|_____|\\____|    \\_/    |________|_____|\\____| |_____|  `.___.'|____| |___|______|   *" << endl << "|                                                                                            |" << endl << "'--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--'" << endl;
 	cout << "     ,--*--*--*--*--*--*--*--*--*--,                    ,--*--*--*--*--*--*--*--*--*--," << endl;
@@ -198,7 +200,7 @@ void inventorystart(double& phealth, double& hhealth, double& pstrenght, int& hc
 		cout << left << setw(4) << fixed << setprecision(1) << phealth / 1000 << " K|";
 	cout << " |                    |           Health  | ";
 	if (hhealth < 1000)
-		cout << " " << left << setw(4) << hhealth << "|";
+		cout << " " << left << setw(5) << hhealth << "|";
 	else if (hhealth == 1000)
 		cout << " " << left << setw(5) << hhealth << "|";
 	else if (hhealth < 10000)
@@ -215,7 +217,7 @@ void inventorystart(double& phealth, double& hhealth, double& pstrenght, int& hc
 	else
 		cout << left << setw(4) << fixed << setprecision(1) << phealth / 1000 << " K|";
 	cout << " *                    * HORSE     Capacity|";
-	cout << right << setw(4) << hcapacity << " Kg| *" << endl << "     |  STAT    Endurance |" << left << setw(4) << pendurance << " %| |                    | -----     Speed   |" << left << setw(5) << hspeed << " %| |" << endl;
+	cout << right << setw(4) << hcapacity << " Kg| *" << endl << "     |  STAT    Endurance |" << left << setw(4) << pendurance << " %| |                    | -----     Speed   |" << right << setw(5) << hspeed << " %| |" << endl;
 	cout << "     *            [";
 	if (level < 10)
 		cout << "00";
@@ -223,44 +225,187 @@ void inventorystart(double& phealth, double& hhealth, double& pstrenght, int& hc
 		cout << "0";
 	cout << level << "]            *                    * STATS     Distance|" << right << setw(4) << hdistance << "  m| *" << endl << "     | [";
 	xpbar(perlevel);
-	cout << "] |                    |    Name: " << right << setw(18) << hname[0] << " |" << endl << "     '--*--*--*--*--*--*--*--*--*--'                    '--*--*--*--*--*--*--*--*--*--'" << endl;
+	cout << "] |                    |    Name: " << right << setw(18) << hname << " |" << endl << "     '--*--*--*--*--*--*--*--*--*--'                    '--*--*--*--*--*--*--*--*--*--'" << endl;
 }
-void invselect(){
-	cout << "                     ,--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--," << endl;
-	cout << "                     |             Weapons |  2. Pockets  5. Hands    |" << endl;
-	cout << "                     * 1. Hotbar:  Potions *  3. Belt     6. Backpack *" << endl;
-	cout << "                     |             Keys    |  4. Sling    7. Horse    |" << endl;
-	cout << "                     '--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--'";
+void pickem(int& slot, bool& inv, bool& select) {
+
+	if (slot < 1)
+		slot = 1;
+	if (slot > 6)
+		slot = 6;
+	cout << "                        ,--*--*--*--*--*--*---*--*--*--*--*--*--," << endl;
+	cout << "                        | ";
+	if (slot == 1)
+		cout << ",---------,";
+	else cout << "           ";
+	cout << "  ";
+	if (slot == 3)
+		cout << ",---------,";
+	else cout << "           ";
+	cout << "  ";
+	if (slot == 5)
+		cout << ",---------,";
+	else cout << "           ";
+	cout << " |" << endl << "                        | ";
+	if (slot == 1)
+		cout << "| Pockets |";
+	else cout << "  Pockets  ";
+	cout << "  ";
+	if (slot == 3)
+		cout << "|  Hands  |";
+	else cout << "   Hands   ";
+	cout << "  ";
+	if (slot == 5)
+		cout << "|  Sling  |";
+	else cout << "   Sling   ";
+	cout << " |" << endl << "                        | ";
+	if (slot == 1)
+		cout << "'---------'";
+	else if(slot == 2)
+		cout << ",---------,";
+	else cout << "           ";
+	cout << "  ";
+	if (slot == 3)
+		cout << "'---------'";
+	else if (slot == 4)
+		cout << ",---------,";
+	else cout << "           ";
+	cout << "  ";
+	if (slot == 5)
+		cout << "'---------'";
+	else if (slot == 6)
+		cout << ",---------,";
+	else cout << "           ";
+	cout << " |" << endl << "                        | ";
+	if (slot == 2)
+		cout << "|  Belt   |";
+	else cout << "   Belt    ";
+	cout << "  ";
+	if (slot == 4)
+		cout << "|Backpack |";
+	else cout << " Backpack  ";
+	cout << "  ";
+	if (slot == 6)
+		cout << "|  Horse  |";
+	else cout << "   Horse   ";
+	cout << " |" << endl << "                        | ";
+	if (slot == 2)
+		cout << "'---------'";
+	else cout << "           ";
+	cout << "  ";
+	if (slot == 4)
+		cout << "'---------'";
+	else cout << "           ";
+	cout << "  ";
+	if (slot == 6)
+		cout << "'---------'";
+	else cout << "           ";
+	cout << " |" << endl << "                        '--*--*--*--*--*--*---*--*--*--*--*--*--'" << endl;
+	cout << "w - up\n" << "s - down\n" << "a - left\n" << "d - right\n" << "f - enter\n" << "e - exit\n";
+	char a;
+	a = _getch();
+	switch (a)
+	{
+	case 'w':
+		slot--;
+		break;
+	case 's':
+		slot++;
+		break;
+	case 'a':
+		slot--;
+		slot--;
+		break;
+	case 'd':
+		slot++;
+		slot++;
+		break;
+	case 'e':
+		inv = false;
+		break;
+	case 'f':
+		select = true;
+		inv = false;
+		break;
+	case 'W':
+		slot--;
+		break;
+	case 'S':
+		slot++;
+		break;
+	case 'A':
+		slot--;
+		slot--;
+		break;
+	case 'D':
+		slot++;
+		slot++;
+		break;
+	case 'E':
+		inv = false;
+		break;
+	case 'F':
+		select = true;
+		inv = false;
+		break;
+	default:
+		break;
+	}
 }
-void invselectslot1_1() {
-	cout << "                     ,--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--," << endl;
-	cout << "                     |           > Weapons |  2. Pockets  5. Hands    |" << endl;
-	cout << "                     * 1. Hotbar:  Potions *  3. Belt     6. Backpack *" << endl;
-	cout << "                     |             Keys    |  4. Sling    7. Horse    |" << endl;
-	cout << "                     '--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--'";
+void pockets() {
+	cout << ",------------------------," << endl;
+	cout << "| Pockets                |" << endl;
+	cout << "|------------------------|" << endl;
+	
 }
-void invselectslot1_2() {
-	cout << "                     ,--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--," << endl;
-	cout << "                     |             Weapons |  2. Pockets  5. Hands    |" << endl;
-	cout << "                     * 1. Hotbar:> Potions *  3. Belt     6. Backpack *" << endl;
-	cout << "                     |             Keys    |  4. Sling    7. Horse    |" << endl;
-	cout << "                     '--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--'";
-}
-void invselectslot1_3() {
-	cout << "                     ,--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--," << endl;
-	cout << "                     |             Weapons |  2. Pockets  5. Hands    |" << endl;
-	cout << "                     * 1. Hotbar:  Potions *  3. Belt     6. Backpack *" << endl;
-	cout << "                     |           > Keys    |  4. Sling    7. Horse    |" << endl;
-	cout << "                     '--*--*--*--*--*--*---*--*--*--*--*--*--*--*--*--'";
+void presmerovanie(int& slot) {
+		if (slot == 1)
+			cout << "slot 1";
+		else if (slot == 2)
+			cout << "slot 2";
+		else if (slot == 3)
+			cout << "slot 3";
+		else if (slot == 4)
+			cout << "slot 4";
+		else if (slot == 5)
+			cout << "slot 5";
+		else if (slot == 6)
+			cout << "slot 6";
+		cout << endl;
+		system("pause");
+} 
+void inventory(double phealth, double pstrenght, double pendurance, double perlevel, int level, int selected, bool hotbar, int slot, bool inv, bool select, double hspeed1[], double hhealth1[], string hname1[], int hcapacity1[], int hdistance1[], int kon, int pockket_coins, bool pckt_coins) {
+	if (pockket_coins == 0)
+		pckt_coins = false;
+	double hhealth = hhealth1[kon - 1];
+	double hspeed = hspeed1[kon - 1];
+	string hname = hname1[kon - 1];
+	int hcapacity = hcapacity1[kon - 1];
+	int hdistance = hdistance1[kon - 1];
+
+	do {
+		select = false;
+		inventorystart(phealth, hhealth, pstrenght, hcapacity, pendurance, hspeed, level, hdistance, perlevel, hname);
+		pickem(slot, inv, select);
+		if (select == true) presmerovanie(slot);
+	} while (inv == true);
 }
 int main() {
-	double phealth = 10500, hhealth = 10500, pstrenght = 10555, pendurance = 93.3, hspeed = 120.2, perlevel = 94.85;
-	int hcapacity = 958, level = 64, hdistance = 400;
-	string hname[13] = { "No horse aquaried","Sergej Baracuda","Napoli Partobona","Mustang Shefchovic","Pablo Paresco","Ishtvan Ludvajovic","Nicolaj Francessco","Valeria Casino","Pepan the Igen","Iron Josif","Benito Soliny","Carina Wieck","Bogdan Boojack" };
-	//	p(health) - player
-	//	h(health) - horse
-	//int selectedhotbar =
-	inventorystart(phealth, hhealth, pstrenght, hcapacity, pendurance, hspeed, level, hdistance, perlevel, hname);
-	invselect();
+	double phealth = 10500, pstrenght = 10555, pendurance = 93.3, perlevel = 94.85;
+	int level = 64, selected = 0;
+	bool hotbar = false;
+	double hspeed1[7] = {0.0,58.0,87.0,38.0,69.0,34.0,85.0 };
+	double hhealth1[7] = { 0,2310,1896,1200,1945,1019,3007 };
+	string hname1[7] = { "No horse aquared","Pablo Paresco","Valeria Casino","Bogdan Boojack","Mustang Schefchovic","Ishtvan Ludvajovic","Pepan the Igen"};
+	int hcapacity1[7] = { 0,64,42,50,54,29,78 };
+	int hdistance1[7] = { 0,865,3290,920,1389,731,5841 };
+	int kon = 1;
+	int slot = 1;
+	bool inv = true;
+	bool select = false;
+//---------------------------------------------
+	int pockket_coins = 10;
+	bool pckt_coins = true;
+	inventory(phealth, pstrenght, pendurance, perlevel, level, selected, hotbar, slot, inv, select, hspeed1, hhealth1, hname1, hcapacity1, hdistance1, kon, pockket_coins, pckt_coins);
 	return 0;
 }
